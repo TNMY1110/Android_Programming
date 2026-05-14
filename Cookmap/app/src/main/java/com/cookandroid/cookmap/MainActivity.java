6676c8075afffba2274d52047844efc3699d82b2
ByeongJo Kim <searain1110@gmail.com>
2026-05-14 16:24:20 +0900

    Exercise 7

--- Cookmap/app/src/main/java/com/cookandroid/cookmap/MainActivity.java
+++ Cookmap/app/src/main/java/com/cookandroid/cookmap/MainActivity.java
@@ -1,31 +1,34 @@
 package com.cookandroid.cookmap;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SubMenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap gMap;
     MapFragment mapFrag;
     GroundOverlayOptions videoMark;
+    ArrayList<GroundOverlay> CCTVList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("구글 지도 활용");
+        setTitle("연습문제 7");
 
         mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
@@ -43,7 +46,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 videoMark = new GroundOverlayOptions().image
                         (BitmapDescriptorFactory.fromResource(R.drawable.presence_video_busy)).
                         position(point, 400f, 400f);
-                gMap.addGroundOverlay(videoMark);
+
+                GroundOverlay overlay = gMap.addGroundOverlay(videoMark);
+                CCTVList.add(overlay);
             }
         });
     }
@@ -53,11 +58,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreateOptionsMenu(menu);
         menu.add(0,1,0,"위성 지도");
         menu.add(0,2,0,"일반 지도");
-
-        SubMenu subMenu = menu.addSubMenu("유명 장소 바로가기");
-        subMenu.add(0, 3, 0, "월드컵경기장");
-        subMenu.add(0, 4, 0, "연희");
-        subMenu.add(0, 5, 0, "광화문");
+        menu.add(0,3,0,"바로전 CCTV 지우기");
+        menu.add(0,4,0,"모든 CCTV 지우기");
 
         return true;
     }
@@ -72,13 +74,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
                 return true;
             case 3:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 13));
+                if(!CCTVList.isEmpty()) {
+                    GroundOverlay last = CCTVList.get(CCTVList.size()-1);
+                    last.remove();
+                    CCTVList.remove(CCTVList.size() - 1);
+                }
                 return true;
             case 4:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.541592, 126.676962), 13));
-                return true;
-            case 5:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.575996, 126.976872), 13));
+                for (GroundOverlay overlay : CCTVList) {
+                    overlay.remove();
+                }
+                CCTVList.clear();
                 return true;
         }
         return false;
